37f80c980565afe05048faeba8f3169e943c8e94
ManFromDelMunky <25533793+ManFromDelMunky@users.noreply.github.com>
2018-10-19 15:17:46 +0100

    Update Create-ADDomain.ps1

--- Create-ADDomain.ps1
+++ Create-ADDomain.ps1
@@ -1,37 +1,44 @@
 ﻿#####################################################################################################################
 # File Name                                                                                                         #
-#   Create-ADDomain.ps1                                                                                            #
+#   Create-ADDomain.ps1                                                                                             #
 # Description                                                                                                       #
-#   Script to Build a Windows domain controller froma a blank VM                                                    #
+#   Script to Build a New Forest and Domain on a blank Windows Server                                               #
+# Prerequisites                                                                                                     #
+#   Server must have an additional disk available minimum size 60 GB                                                #
 # Usage                                                                                                             #
 #   Copy and paste script in to PowerShell window localy in the virtual machie                                      #
 #   Will need to save the AD Safe mode password somewhere                                                           #
 # Scope                                                                                                             #
-#   Server must be minimum 2012 R2                                                                                  #
+#   Server must be minimum Windows Server 2012 R2                                                                   #
 #   Blank server with  disks attached, second disk must be < 60 GB                                                  #
 # Change Control                                                                                                    #
 #   Andy Ferguson 18/04/2018 Initial Version                                                                        #
 # To Do                                                                                                             #
-# 
-# Source info
-#
+#                                                                                                                   #
+# Source info                                                                                                       #
+#                                                                                                                   #
 #####################################################################################################################
 
+# Variables that can be changed to suit
+# NETBIOS Name for the Domain
+$Domain = "Domain"
+# DNS Domain Name will append .loc for the DNS name ammend to suit
+$DomainFQDN = "$Domain.loc"
+
 #Get-WindowsFeature
+# Install Feature
 Install-WindowsFeature AD-Domain-Services,GPMC,RSAT-AD-Tools
+# Create new partitions for SYSVol AD Logs and Database
 Get-Disk | Where-Object PartitionStyle -Eq "RAW" | Initialize-Disk
 $SysvolDisk = New-Partition -DiskNumber 2 -Size 20GB -AssignDriveLetter | Format-Volume -FileSystem NTFS -NewFileSystemLabel SYSVOL -Confirm -Force
-New-Partition -DiskNumber 2 -Size 20GB -DriveLetter "L" | Format-Volume -FileSystem NTFS -NewFileSystemLabel SYSVOL -Confirm -Force
-New-Partition -DiskNumber 2 -UseMaximumSize -DriveLetter "S" | Format-Volume -FileSystem NTFS -NewFileSystemLabel SYSVOL -Confirm -Force
+New-Partition -DiskNumber 2 -Size 20GB -DriveLetter "L" | Format-Volume -FileSystem NTFS -NewFileSystemLabel ADLogs -Confirm -Force
+New-Partition -DiskNumber 2 -UseMaximumSize -DriveLetter "S" | Format-Volume -FileSystem NTFS -NewFileSystemLabel ADDB -Confirm -Force
 
 #New-Partition -DiskNumber 2 -Size 
-
-
 #Get-Partition -DiskNumber "2" -PartitionNumber "2" | Remove-Partition -Confirm $true
 
-$Domain = "Domain"
 $SysvolPath = $SysvolDisk.DriveLetter + ':\SYSVOL'
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "S:\NTDS" -DomainMode "7" -DomainName "$Domain.loc" -DomainNetbiosName "$Domain" `
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "S:\NTDS" -DomainMode "7" -DomainName "$DomainFQDN" -DomainNetbiosName "$Domain" `
  -ForestMode "7" -InstallDns:$true -LogPath "L:\NTDS" -NoRebootOnCompletion:$false -SysvolPath "$SysvolPath" -Force:$true
 
- #Add Safe Mode Admin password
+ #Add Safe Mode Admin password to pop up box
